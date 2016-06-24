350aaa6447bede98dce37e31a3328b971c48a601
William Stevens <contact@wastevensv.com>
2016-06-23 21:24:29 -0400

    Added nim AI. Always wins.

---
+++ C-like/nim/README.md
@@ -0,0 +1,8 @@
+# What is this?
+A C program that wins the game of [nim].
+
+# Why?
+I was bored. I wanted to keep my C skills fresh in my mind. I saw this [video].
+
+[nim]:    https://en.wikipedia.org/wiki/Nim
+[video]:  https://youtu.be/9KABcmczPdg
---
+++ C-like/nim/nim.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <time.h>
+
+#define START_COINS   12
+#define MAX_PICK 3
+
+int main(int argc, char *argv[]) {
+  int coins = START_COINS;
+  while(coins != 0) {
+    for(int c=0; c < coins; c++) {
+      fputs("#", stdout);
+      fflush(stdout);
+    }
+    fputs("\n", stdout);
+    char user = getchar() - '0';
+    getchar();
+    if(user < 1 || user > 3) {
+      printf("Invalid Choice %d.\n", user);
+      return -1;
+    } else {
+      printf("You took %d coins.\n", user);
+      coins -= user;
+    }
+    for(int c=0; c < coins; c++) {
+      fputs("#", stdout);
+      fflush(stdout);
+    }
+    fputs("\n", stdout);
+    char nim = 4 - user;
+    printf("Nim took %d coins.\n", nim);
+    coins -= nim;
+  }
+  fputs("Nim wins.\n", stdout);
+  return 0;
+}
